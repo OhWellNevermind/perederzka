d4da6d415a56947407f869fd3776fc24ce715827
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-10-05 17:17:00 +0300

    burger menu added

---
+++ src/components/Header/BurgerMenu.jsx
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+
+const BurgerMenu = ({ isOpen }) => {
+  return (
+    <div
+      className={`z-50 absolute transition-all ease-in-out duration-300 w-full h-full bg-white top-[56px] translate-x-full left-0 ${
+        isOpen ? "translate-x-0" : "translate-x-full hidden"
+      } `}
+    >
+      <div className="flex flex-col items-center h-full">
+        <div className="flex flex-col pt-[20px] px-[30px] gap-[30px]">
+          <a className="font-semibold text-sm text-black text-center" href="">
+            Залишити тварину на перетримку
+          </a>
+          <a className="font-semibold text-sm text-black text-center" href="">
+            Стати догсітером
+          </a>
+          <a className="font-semibold text-sm text-black text-center" href="">
+            FAQs
+          </a>
+        </div>
+        <div className="mt-[141px] flex gap-[30px]">
+          <button className="font-semibold text-lg text-main">Увійти</button>
+          <button className="p-[10px] bg-main rounded-[5px] font-semibold text-lg text-white hover:bg-hover focus:bg-white focus:text-hover">
+            Зареєструватися
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+BurgerMenu.propTypes = {
+  isOpen: PropTypes.bool,
+};
+
+export default BurgerMenu;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,43 @@
 import { HiOutlineMenu } from "react-icons/hi";
+import { IoCloseSharp } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import BurgerMenu from "./BurgerMenu";
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header className="bg-[#FBFBFB]  border-b-2 border-b-[#D9D9D9] px-[10px]">
-      <div className="flex justify-between">
-        <Link to="/" className="font-black text-2xl text-main">
-          LOGO
-        </Link>
-        <button type="button">
-          <HiOutlineMenu size={24} fill="#17BBDE" stroke="#17BBDE" />
-        </button>
-      </div>
-    </header>
+    <>
+      <header className="bg-[#FBFBFB]  border-b-2 border-b-[#D9D9D9] p-[10px]">
+        <div className="flex justify-between">
+          <Link to="/" className="font-black text-2xl text-main">
+            LOGO
+          </Link>
+          {isOpen ? (
+            <button
+              onClick={() => {
+                setIsOpen(false);
+                window.document.documentElement.style.overflowY = "auto";
+              }}
+            >
+              <IoCloseSharp size={24} fill="#17BBDE" stroke="#17BBDE" />
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                setIsOpen(true);
+                window.document.documentElement.style.overflowY = "hidden";
+              }}
+              type="button"
+            >
+              <HiOutlineMenu size={24} fill="#17BBDE" stroke="#17BBDE" />
+            </button>
+          )}
+        </div>
+      </header>
+      <BurgerMenu isOpen={isOpen} />
+    </>
   );
 };
 
